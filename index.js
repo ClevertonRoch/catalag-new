7aa0cbe9276673420d6d23279cca7c9edccf28a7
ClevertonRoch <websistematizando@gmail.com>
2023-05-29 13:03:14 -0300

    feat: create and config file routes/index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const router = require('./routes')
 
 
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.get('')
+app.get('/', router)
 
 app.listen(8080,()=> console.log('server running!'))
